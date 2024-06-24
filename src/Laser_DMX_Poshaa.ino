70ccb5a5dfb5cd554ac39dd2f3b511d77e8e360a
Clément Delestre <clementdelestre@gmail.com>
2024-06-25 00:28:31 +0200

    update

--- src/Laser_DMX_Poshaa.ino
+++ src/Laser_DMX_Poshaa.ino
@@ -1,54 +1,75 @@
-/* This example joins two useful libraries for ESP8266 WiFi Module
- * Websockets library for interfacing from either web interface or
- * some external application and DMX library for controlling
- * devices on a DMX bus.
- *
- * Install these libraries:
- * https://github.com/Links2004/arduinoWebSockets
- * https://github.com/Rickgg/ESP-Dmx
- *
- * Web interface allow one to set the channel and control an RGB
- * light, assuming channels for cors follow one after eachother
- *
- * How to use:
- * 1) Change WiFi settings accordingly to connect to your network
- * 2) Connect an RS485 driver chip to GPIO02( D4 on Nodemcu 1.0)
- * 3) Check out the serial output for IP or
- * visit http://rgbdmx.local if your device supports mDNS and
- * you are in the same local network
-
-Copyright Institute IRNAS Rače 2016 - info@irnas.eu
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
-
 #include <Arduino.h>
 #include <ESPDMX.h>
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServerSecure.h>
+
+
 #include <ESP8266mDNS.h>
+
+
 #include <WebSocketsServer.h>
 
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
+// #include <ESPAsyncTCP.h>
+// #include <ESPAsyncWebServer.h>
 #include <FS.h>
+
+#include "WebSockets4WebServerSecure.h"
+
+static const char tls_key[] PROGMEM =  R"EOF(
+-----BEGIN PRIVATE KEY-----
+MIIC/DCCAeSgAwIBAgIQXoOzPdQTXZhN674sd44k2zANBgkqhkiG9w0BAQsFADAR
+MQ8wDQYDVQQDDAZyZ2JkbXgwHhcNMjQwNjI0MjAzNjUzWhcNMjUwNjI0MjA1NjUz
+WjARMQ8wDQYDVQQDDAZyZ2JkbXgwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK
+AoIBAQCzNBDFsRsEBOZ5JxTDvC3SD2iS8cNKZnC5HPLXaYVfXEMDYLXPHOwaoTtw
+QAGIbl9DYFRjHpLifmrOFdDfJQqdCf8s27U4fGRQ0lUjBvQZaxbhT1eqX/m3KPtN
+cJ49XjaT74l9wH5BozrNqsV1ssPm9iuPHQlpekhBkkJqkjrE2E/QoJkkSgEHCQsR
+g1hSK1jGPvYyuCRkZSmfmX/poUEk+w7oHZDmyt0LoxPuvQt9m9VmKJraWitrNijZ
+R4v/c2384hbitgR7MPPLTpfDsGxNbd2bsfF/KYVE/m1cwE1LVq1rnPPMqQZwRasv
+8qCa9F9z+U4gYXUbdAamS7cYlZUFAgMBAAGjUDBOMA4GA1UdDwEB/wQEAwIFoDAd
+BgNVHSUEFjAUBggrBgEFBQcDAgYIKwYBBQUHAwEwHQYDVR0OBBYEFPgKgG1UpxQI
+Z5Nn3R/IRcNsB8ijMA0GCSqGSIb3DQEBCwUAA4IBAQCwz4xe7f1WemM060WWzV9z
+n6nfMtMrA+P43ctIykbvDZ/AFVEFeXyj2nWEfCeZkarb0BxlGNI6+6uf2Q8KeLB3
+vjaXc5t6r3ZbAhQb6dd2DwHfIPlSpZ/nBx4ljUet83M37A8dwVHjCCAokKr+4ZTW
+qpn5v7lrh1LY6pWN5uLsYALErMFnIm6m9hw/m+fJPVg59gayQ2JF69MwXQqvMR5X
+53kaUkq+IWIfwoD0cCaSRgKt+AvR4wI6MSq5oUwTqIdBrJRJce0+72qi/i/Hg9b1
+cPGArElCV2V5X5m4CQLz3hge/n8uxIm9yKp6wvycpGtJHBwMheN2wXQkKmJO84X5
+-----END PRIVATE KEY-----
+)EOF";
+
+static const char tls_cert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIC/DCCAeSgAwIBAgIQXoOzPdQTXZhN674sd44k2zANBgkqhkiG9w0BAQsFADAR
+MQ8wDQYDVQQDDAZyZ2JkbXgwHhcNMjQwNjI0MjAzNjUzWhcNMjUwNjI0MjA1NjUz
+WjARMQ8wDQYDVQQDDAZyZ2JkbXgwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK
+AoIBAQCzNBDFsRsEBOZ5JxTDvC3SD2iS8cNKZnC5HPLXaYVfXEMDYLXPHOwaoTtw
+QAGIbl9DYFRjHpLifmrOFdDfJQqdCf8s27U4fGRQ0lUjBvQZaxbhT1eqX/m3KPtN
+cJ49XjaT74l9wH5BozrNqsV1ssPm9iuPHQlpekhBkkJqkjrE2E/QoJkkSgEHCQsR
+g1hSK1jGPvYyuCRkZSmfmX/poUEk+w7oHZDmyt0LoxPuvQt9m9VmKJraWitrNijZ
+R4v/c2384hbitgR7MPPLTpfDsGxNbd2bsfF/KYVE/m1cwE1LVq1rnPPMqQZwRasv
+8qCa9F9z+U4gYXUbdAamS7cYlZUFAgMBAAGjUDBOMA4GA1UdDwEB/wQEAwIFoDAd
+BgNVHSUEFjAUBggrBgEFBQcDAgYIKwYBBQUHAwEwHQYDVR0OBBYEFPgKgG1UpxQI
+Z5Nn3R/IRcNsB8ijMA0GCSqGSIb3DQEBCwUAA4IBAQCwz4xe7f1WemM060WWzV9z
+n6nfMtMrA+P43ctIykbvDZ/AFVEFeXyj2nWEfCeZkarb0BxlGNI6+6uf2Q8KeLB3
+vjaXc5t6r3ZbAhQb6dd2DwHfIPlSpZ/nBx4ljUet83M37A8dwVHjCCAokKr+4ZTW
+qpn5v7lrh1LY6pWN5uLsYALErMFnIm6m9hw/m+fJPVg59gayQ2JF69MwXQqvMR5X
+53kaUkq+IWIfwoD0cCaSRgKt+AvR4wI6MSq5oUwTqIdBrJRJce0+72qi/i/Hg9b1
+cPGArElCV2V5X5m4CQLz3hge/n8uxIm9yKp6wvycpGtJHBwMheN2wXQkKmJO84X5
+-----END CERTIFICATE-----
+)EOF";
 
 DMXESPSerial dmx;
 
 ESP8266WiFiMulti wiFiMulti;
-AsyncWebServer server(80);
-WebSocketsServer webSocket = WebSocketsServer(81);
+// WebSocketsServer webSocket = WebSocketsServer(81);
 
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+
+BearSSL::ESP8266WebServerSecure server(443);
+WebSockets4WebServerSecure webSocket_secure;
+
+void websocket_handle_event(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 {
     switch (type) {
 
@@ -58,7 +79,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
         case WStype_CONNECTED:
             Serial.println("Client connected!");
             // send message to client
-            webSocket.sendTXT(num, "!Connected");
+            // webSocket.sendTXT(num, "!Connected");
             sendConfig(num);
 
             break;
@@ -118,7 +139,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 
                 //transform the payload to a request for all clients
                 payload[0] = '?';
-                webSocket.broadcastTXT(payload, length);
+                // webSocket.broadcastTXT(payload, length);
             }
 
             if (payload[0] == '?'){
@@ -146,7 +167,7 @@ void sendConfig(uint8_t num){
     f.close();
 
     String resp =  String("?" + json);
-    webSocket.sendTXT(num, resp);
+    // webSocket.sendTXT(num, resp);
 }
 
 void setup()
@@ -176,9 +197,6 @@ void setup()
         Serial.print(++i); Serial.print(' ');
     }
 
-    // start webSocket server
-    webSocket.begin();
-    webSocket.onEvent(webSocketEvent);
 
     if (MDNS.begin("rgbdmx"))
     {
@@ -186,25 +204,10 @@ void setup()
         MDNS.addService("http", "tcp", 80);
     }
 
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        Serial.println("GET /");
-        request->send(SPIFFS, "/index.html", "text/html"); 
-    });
+    MDNS.addService("https", "tcp", 443);
+    MDNS.addService("wss", "tcp", 443);
 
-    server.on("/*", HTTP_GET, [](AsyncWebServerRequest *request){
-        String path = request->url();
-        Serial.println("Requested URL: " + path);
-
-        if(SPIFFS.exists(path)){
-            request->send(SPIFFS, path);
-        } else {
-            request->send(404, "text/plain", "File Not Found");
-        }
-    });
-
-    server.begin();
-    MDNS.addService("http", "tcp", 80);
-    MDNS.addService("ws", "tcp", 81);
+    setup_http();
 
     dmx.init(10); // initialize with bus length
 
@@ -215,9 +218,71 @@ void setup()
     Serial.println(" dBm");
 }
 
+void setup_http(void) {
+    BearSSL::X509List *tls_chain;
+
+    tls_chain = new BearSSL::X509List(tls_cert);
+    //tls_chain->append(tls_cert_intermediate);
+    //tls_chain->append(tls_cert_root);
+    server.getServer().setRSACert(tls_chain, new BearSSL::PrivateKey(tls_key));
+
+    server.on("/", http_handle_root); 
+    server.onNotFound(http_handle_404);
+
+    // And here is how we make the websocket pass through the actual server, so we can (re)use SSL and all that
+    server.addHook(webSocket_secure.hookForWebserver("/socket", websocket_handle_event));
+
+    server.begin();
+    Serial.println("HTTPS server started");
+}
+
+void http_handle_root(void) {
+    Serial.println("HTTP request received");
+    server.sendHeader("Cache-Control", "no-cache");
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", "<!DOCTYPE html><html><body>Some HTML page where you can interact with the websocket</body></html>");
+    // The above HTML should contain some JavaScript for connecting to the socket, like this: new WebSocket('wss://' + document.location.host + '/socket');
+    // The '/socket' at the end corresponds to the value from the myweb_server.addHook() call
+}
+
+void http_handle_404(void) {
+    Serial.println("HTTP 404 request received");
+    server.sendHeader("Cache-Control", "no-cache");
+    server.sendHeader("Connection", "close");
+    server.send(404, "text/html", "<!DOCTYPE html><html><body>Some HTML page indicating a 404 error</body></html>");
+}
+
+void http_handle_root2() {
+    String path = server.uri();
+    Serial.println("Requested URL: " + path);
+
+    if (SPIFFS.exists(path)) {
+        File file = SPIFFS.open(path, "r");
+        size_t sent = server.streamFile(file, getContentType(path));
+        file.close();
+    } else {
+        server.send(404, "text/plain", "File Not Found");
+    }
+}
+
+String getContentType(String filename) {
+    if (filename.endsWith(".html") || filename.endsWith(".htm")) return "text/html";
+    else if (filename.endsWith(".css")) return "text/css";
+    else if (filename.endsWith(".js")) return "application/javascript";
+    else if (filename.endsWith(".png")) return "image/png";
+    else if (filename.endsWith(".gif")) return "image/gif";
+    else if (filename.endsWith(".jpg") || filename.endsWith(".jpeg")) return "image/jpeg";
+    else if (filename.endsWith(".ico")) return "image/x-icon";
+    else if (filename.endsWith(".xml")) return "text/xml";
+    else if (filename.endsWith(".pdf")) return "application/pdf";
+    else if (filename.endsWith(".zip")) return "application/zip";
+    return "application/octet-stream"; // Default content type
+}
+
 void loop()
 {
-    webSocket.loop();
+    server.handleClient();
+    webSocket_secure.loop();
     dmx.update();
     MDNS.update();
 }
