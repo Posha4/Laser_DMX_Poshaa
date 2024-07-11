74d83c8f280740144025cecbe460ff7af220964d
Clément Delestre <clementdelestre@gmail.com>
2024-07-11 14:55:46 +0200

    update

--- src/README.md
+++
@@ -1,2 +0,0 @@
-# Laser_DMX_Poshaa
- ESP8266 code to operate DMX fixtures
--- src/fullupload.bat
+++
@@ -1,3 +0,0 @@
-C:\Users\cleme\.platformio\penv\Scripts\platformio.exe run --target buildfs
-
-C:\Users\cleme\.platformio\penv\Scripts\platformio.exe run --target uploadfs
